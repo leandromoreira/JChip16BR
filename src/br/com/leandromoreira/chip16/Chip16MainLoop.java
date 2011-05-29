1f3ed86e59d08c2e9b05cae7eb438367085b480d
leandro.rhcp99@gmail.com <>
2011-05-29 00:00:07 -0300

    more refactorings

--- src/br/com/leandromoreira/chip16/Chip16MainLoop.java
+++ src/br/com/leandromoreira/chip16/Chip16MainLoop.java
@@ -1,8 +1,6 @@
 package br.com.leandromoreira.chip16;
 
 import br.com.leandromoreira.chip16.gpu.Render;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * @author leandro-rm
@@ -11,15 +9,6 @@ public class Chip16MainLoop implements Runnable {
 
     private Chip16Machine machine;
     private Render render;
-
-    public Chip16MainLoop(Chip16Machine machine, Render render) {
-        this.machine = machine;
-        this.render = render;
-    }
-    //1000000/60 = 16666.666666667 CPU cycles for each vblank
-    // each vblank should occour each 16ms
-    // scanline each 16666.666666667 / 320 = 52.083333333
-    //vblank interrupt (320-240)*16666.666666667/320 = 4166.666666667 cycles
     private final static int FPS = 60;
     private final static int CPU_SPEED = 1000000;
     private final static double INSTRUCTIONS_PER_VBLANK = CPU_SPEED / FPS;
@@ -29,6 +18,11 @@ public class Chip16MainLoop implements Runnable {
     private long startTime;
     private long currentTime;
     private long diffirence;
+
+    public Chip16MainLoop(Chip16Machine machine, Render render) {
+        this.machine = machine;
+        this.render = render;
+    }
 
     @Override
     public void run() {
